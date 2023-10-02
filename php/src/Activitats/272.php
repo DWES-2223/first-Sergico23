a7a13ae029071783f7a11cfe37c6442c068c864e
SergiColoma <sergicoloma23@gmail.com>
2023-10-02 12:04:31 +0200

    270-271

--- php/src/Activitats/272.php
+++ php/src/Activitats/272.php
@@ -1,1 +1,16 @@
 <?php
+function vell($atletas) {
+    $oldestDate = strtotime('9999-12-31');
+    $oldestKey = null;
+
+    foreach ($atletas as $atleta => $datos) {
+        $fecha = strtotime(str_replace('.', '-', $datos['data']));
+
+        if ($fecha < $oldestDate) {
+            $oldestDate = $fecha;
+            $oldestKey = $atleta;
+        }
+    }
+
+    return $oldestKey;
+}
